a07c855d9cd45370556a32ee7d4a8545412f082c
your zahra <gholizahdez0681@gmail.com>
2024-10-26 09:38:19 +0330

    sendverificationEmail

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Jobs\SendVerificationEmailJob;
+
+
+class RegisterController extends Controller
+{
+    public function register()
+    {
+//        $validatedData = $this->validate(request(), [
+//           'name' => 'required|min:3',
+//           'email' => 'required|email|unique:users,email',
+//            'password' => 'required|min:3'
+//        ]);
+
+        $validatedData = [
+            'name' => 'zahra',
+            'email' => 'zahra@gmail.com',
+            'password' => '123456789'
+        ];
+
+        $user = \App\Models\User::create($validatedData);
+
+        SendVerificationEmailJob::dispatch($user);
+
+
+        return response()->json([
+            'status' => 'success'
+        ]);
+    }
+}
